614bacb342149f1b2481a8c26dba425e16f296be
Zamin <zfnoorani@noctrl.edu>
2021-03-23 21:53:22 +0000

    added 2 screens to stack navigator

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import LostForm from './Apps/screens/LostForm';
 import SignInScreen from './Apps/screens/SignInScreen';
 import Flyer from './Apps/screens/Flyer';
+import FoundPost from './Apps/screens/FoundPost';
+
+import MenuScreen from './Apps/screens/MenuScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
  
@@ -14,9 +17,11 @@ function App() {
   return (
     <NavigationContainer>
  
-    <Stack.Navigator initialRouteName="LostForm" >
+    <Stack.Navigator initialRouteName="FoundPost" >
     <Stack.Screen name="Flyer" component={Flyer} options={{title: 'Flyer'}} />
- 
+    <Stack.Screen name="MenuScreen" component={MenuScreen} options={{title: 'MenuScreen'}} />
+    <Stack.Screen name="FoundPost" component={FoundPost} options={{title: 'FoundPost'}} />
+
     <Stack.Screen name="LostForm" component={LostForm}  options={{title: 'welcome'}} />
     <Stack.Screen name="SignInScreen" component={SignInScreen} options={{title: 'no'}} />
  
