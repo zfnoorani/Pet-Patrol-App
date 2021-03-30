e765406baa4818f44e2308dd33be2ab04c8ae8a3
zfnoorani <zfnoorani@noctrl.edu>
2021-03-30 14:32:05 -0500

    playing around with forms

--- Apps/screens/FoundPost.js
+++ Apps/screens/FoundPost.js
@@ -1,34 +1,38 @@
-import React, {useState} from 'react';
-import {Text, StyleSheet, View, TextInput, Button,ImageBackground, Keyboard} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  ImageBackground,
+  Keyboard,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { database } from "../../fbconfig";
 
-
-
-const App = ({navigation}) => {
-    const [pet, setPet] = useState('Dog');
-    const [time, setTime] = useState('<1 hour');
-    const [color, setColor] = useState('Black');
-    const [name, setName] = useState('FirstName');
-    const [lastName, setLastName] = useState('LastName');
-    const [danger, setDanger] = useState('Dangerous');
-    const [breed, setBreed] = useState('Unknown')
-    const [info, setInfo] = useState('Unknown')
-    const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
+const App = ({ navigation }) => {
+  const [pet, setPet] = useState("Dog");
+  const [time, setTime] = useState("<1 hour");
+  const [color, setColor] = useState("Black");
+  const [name, setName] = useState("FirstName");
+  const [lastName, setLastName] = useState("LastName");
+  const [danger, setDanger] = useState("Dangerous");
+  const [breed, setBreed] = useState("Unknown");
+  const [info, setInfo] = useState("Unknown");
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
-  
-  var tempDate="Ddate";
+
+  var tempDate = "Ddate";
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
+    setShow(Platform.OS === "ios");
     setDate(currentDate);
-    tempDate=currentDate;
-    console.log(currentDate)
-    
-    
-
+    tempDate = currentDate;
+    console.log(currentDate);
   };
 
   const showMode = (currentMode) => {
@@ -37,51 +41,44 @@ const App = ({navigation}) => {
   };
 
   const showDatepicker = () => {
-    showMode('date');
+    showMode("date");
   };
 
   const showTimepicker = () => {
-    showMode('time');
+    showMode("time");
   };
-    
 
-    
+  console.log(lastName);
 
-    console.log(lastName)
-    
-    
+  const readdata = () => {
+    var starCountRef = database.ref("/");
+    starCountRef.on("value", (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+  };
 
-   
-   
-    
-   
-    return (
-      
-          
-      <ImageBackground
-      style={styles.background} 
-      source={require('../assets/pawprints.jpg')}>
-        
-      <View >
-      
-    
-        <Text >Find Pet Form </Text>
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/pawprints.jpg")}
+    >
+      <View>
+        <Text>Find Pet Form </Text>
         <View>
-          <TextInput 
+          <TextInput
             placeholder="First Name"
             selectedValue={name}
-            onChangeText={name => setName(name)} />
+            onChangeText={(name) => setName(name)}
+          />
           <TextInput
-             placeholder="Last Name"
-             selectedValue={lastName}
-             onChangeText={lastName => setLastName(lastName)} />
-          
-           <Text style={styles.baseText}>
-            Type of Pet
-           </Text>
-            <Picker
-            selectedValue={pet}
-            onValueChange={pet => setPet(pet)}>
+            placeholder="Last Name"
+            selectedValue={lastName}
+            onChangeText={(lastName) => setLastName(lastName)}
+          />
+
+          <Text style={styles.baseText}>Type of Pet</Text>
+          <Picker selectedValue={pet} onValueChange={(pet) => setPet(pet)}>
             <Picker.Item label="Dog" value="Dog" />
             <Picker.Item label="Cat" value="Cat" />
             <Picker.Item label="Snake" value="Snake" />
@@ -89,96 +86,97 @@ const App = ({navigation}) => {
             <Picker.Item label="Mouse" value="Mouse" />
             <Picker.Item label="NGN" value="Naira" />
           </Picker>
-          <Text style={styles.baseText}>
-            When did you find it?
+          <Text style={styles.baseText}>When did you find it?</Text>
 
-            </Text>
+          <View>
+            <Button onPress={showDatepicker} title="Enter Date Pet was found" />
+          </View>
+          <View>
+            <Button onPress={showTimepicker} title="Enter Time Pet was found" />
+          </View>
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              display="default"
+              onChange={onChange}
+            />
+          )}
 
-      <View>
-        <Button onPress={showDatepicker} title="Enter Date Pet was found" />
-      </View>
-      <View>
-        <Button onPress={showTimepicker} title="Enter Time Pet was found"/>
-      </View>
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}          
-          
-
-          
-        />
-      )}
-          
-          <Text style={styles.baseText}>
-            How Dangerous was the pet?
-           </Text>
+          <Text style={styles.baseText}>How Dangerous was the pet?</Text>
           <Picker
-          selectedValue= {danger}
-            onValueChange={danger => setDanger(danger)}>
-          <Picker.Item label = "Harmless"></Picker.Item>
-          <Picker.Item label = "Mostly Harmless"></Picker.Item>
-          <Picker.Item label = "Dangerous"></Picker.Item>
-          <Picker.Item label = "Very Dangerous"></Picker.Item>
-      </Picker>
-      <Text style={styles.baseText}>
-          What Color was the pet you found?        
-        </Text>
-        
-    
-   
-      <Picker
-          selectedValue={color}
-          onValueChange={pet => setColor(pet)}>
-        <Picker.Item label = "Black"></Picker.Item>
-        <Picker.Item label = "Red"></Picker.Item>
-        <Picker.Item label = "Brown"></Picker.Item>
-        <Picker.Item label = "White"></Picker.Item>
-        <Picker.Item label = "Grey"></Picker.Item>
-        <Picker.Item label = "Blue"></Picker.Item>
-        <Picker.Item label = "Blonde"></Picker.Item>
-        <Picker.Item label = "Other"></Picker.Item>
-      </Picker>
-      <TextInput placeholder='Breed' selectedValue={breed} onChangeText={breed => setBreed(breed)} />
-      <TextInput style={styles.textify} placeholder='What town did you find the pet in?' onSubmitEditing={Keyboard.dismiss} selectedValue={info} onChangeText={info => setInfo(info)}/>
+            selectedValue={danger}
+            onValueChange={(danger) => setDanger(danger)}
+          >
+            <Picker.Item label="Harmless"></Picker.Item>
+            <Picker.Item label="Mostly Harmless"></Picker.Item>
+            <Picker.Item label="Dangerous"></Picker.Item>
+            <Picker.Item label="Very Dangerous"></Picker.Item>
+          </Picker>
+          <Text style={styles.baseText}>What Color was the pet you found?</Text>
 
-      <TextInput style={styles.textify} placeholder='Additional Info' onSubmitEditing={Keyboard.dismiss} selectedValue={info} onChangeText={info => setInfo(info)}/>
-        <Button
+          <Picker selectedValue={color} onValueChange={(pet) => setColor(pet)}>
+            <Picker.Item label="Black"></Picker.Item>
+            <Picker.Item label="Red"></Picker.Item>
+            <Picker.Item label="Brown"></Picker.Item>
+            <Picker.Item label="White"></Picker.Item>
+            <Picker.Item label="Grey"></Picker.Item>
+            <Picker.Item label="Blue"></Picker.Item>
+            <Picker.Item label="Blonde"></Picker.Item>
+            <Picker.Item label="Other"></Picker.Item>
+          </Picker>
+          <TextInput
+            placeholder="Breed"
+            selectedValue={breed}
+            onChangeText={(breed) => setBreed(breed)}
+          />
+          <TextInput
+            style={styles.textify}
+            placeholder="What town did you find the pet in?"
+            onSubmitEditing={Keyboard.dismiss}
+            selectedValue={info}
+            onChangeText={(info) => setInfo(info)}
+          />
+
+          <TextInput
+            style={styles.textify}
+            placeholder="Additional Info"
+            onSubmitEditing={Keyboard.dismiss}
+            selectedValue={info}
+            onChangeText={(info) => setInfo(info)}
+          />
+          <Button
             title="Submit"
-   
             onPress={() => {
-              navigation.navigate('Flyer', {
+              navigation.navigate("Flyer", {
                 name: name,
                 lastName: lastName,
                 itemName: pet,
                 timeStamp: time,
                 colorName: color,
-                danger: danger, 
+                danger: danger,
                 breed: breed,
-                info: info
-   
-                
+                info: info,
               });
             }}
-    />
-   
+          />
+          <Button
+            onpress={() => {
+              readdata();
+            }}
+          />
         </View>
       </View>
-      </ImageBackground>
-   
-    );
-  };
-  const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        resizeMode: 'contain'
-        
-    }
-  });
-  
+    </ImageBackground>
+  );
+};
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+});
 
 export default App;
---
+++ fbconfig/index.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/database";
+import "firebase/storage";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDiAg9hd1bojV49EZw90ZvapLWBo7JSF8I",
+  authDomain: "petpatrol-5f189.firebaseapp.com",
+  projectId: "petpatrol-5f189",
+  storageBucket: "petpatrol-5f189.appspot.com",
+  messagingSenderId: "1097247684276",
+  appId: "1:1097247684276:web:7ea2af44f675e73cd6c9e1",
+  measurementId: "G-2KWN13S27M",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const auth = firebase.auth();
+
+export { database, firebase as default };
