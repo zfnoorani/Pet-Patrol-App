5ee4ed9e37c3d5b8c6bcc64590a29a858b5bdbed
ktsullivan0523 <ktsullivan@noctrl.edu>
2021-04-05 10:20:19 -0500

    sign up screen added

--- Apps/screens/SignUpNewScreen.js
+++ Apps/screens/SignUpNewScreen.js
@@ -71,39 +71,6 @@ render(){
 
 }
 }
-function signUpSub(username, password, firstName, lastName, city, state){
-    const app = express();
-
-    connection.getConnection()
-        .then(conn => {
-        
-        conn.query("SELECT 1 as val")
-            .then((rows) => {
-            console.log(rows); //[ {val: 1}, meta: ... ]
-            //Table must have been created before 
-            // " CREATE TABLE myTable (id int, val varchar(255)) "
-            where = ' (?, ?, ?, ?, ?, ?)'
-            values = [username, password, firstName, lastName, city, state]
-            var sql = "INSERT INTO Users (username, password, fname, lname, city, state) VALUES" + where
-            return conn.query(sql, values);
-
-            })
-            .then((res) => {
-            console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-            conn.end();
-            })
-            .catch(err => {
-            //handle error
-            console.log(err); 
-            conn.end();
-            })
-            
-        }).catch(err => {
-        //not connected
-        });
-
-}
-
 
 
 const styles = StyleSheet.create({
